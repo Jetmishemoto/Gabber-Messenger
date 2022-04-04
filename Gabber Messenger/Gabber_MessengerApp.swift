5fd579a7b65be92aa01256901123394ba67f61f1
Daadoni C <Daadoniwp92@gmx.com>
2022-04-04 16:24:31 -0500

    Initial Commit

---
+++ Gabber Messenger/Gabber_MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gabber_MessengerApp.swift
+//  Gabber Messenger
+//
+//  Created by daadoni coggins on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Gabber_MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
